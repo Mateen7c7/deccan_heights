fe4745e5418f0d94a03352c15f6475f14ca0b32e
Mateen Azfar <mateenazfar786@gmail.com>
2025-12-29 21:55:27 +0530

    Update venture area and price details.

--- app/ventures/page.tsx
+++ app/ventures/page.tsx
@@ -14,8 +14,8 @@ const ongoingVentures = [
     image: "/plot1.webp",
     link: "/ventures/phase-1",
     stats: [
-      { label: "Area", value: "1200 Sq.ft" },
-      { label: "Price", value: "₹25L+" },
+      { label: "Area", value: "9 Acres" },
+      { label: "Price", value: "₹8500 " },
       { label: "Location", value: "Shadnagar" },
     ],
   },
