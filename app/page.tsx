a82a995c8c1b63517058cc34c9f611c2e1aee9b8
Mateen <mateenazfar786@gmail.com>
2026-07-28 18:36:54 +0530

    feat: implement homepage layout with sections for hero, ventures, services, and testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import ActiveVenturesSection from "@/components/home/ActiveVenturesSection";
 import Hero from "@/components/Hero";
 import TrustValue from "@/components/TrustValue";
-import LeadCaptureStrip from "@/components/LeadCaptureStrip";
+// import LeadCaptureStrip from "@/components/LeadCaptureStrip";
 import BenefitsSection from "@/components/home/BenefitsSection";
 import FoundersSection from "@/components/home/FoundersSection";
 // import OurServicess from "@/components/home/OurServicess";
@@ -18,7 +18,7 @@ export default function Home() {
       <ActiveVenturesSection />
       <BenefitsSection />
       <FoundersSection />
-      <LeadCaptureStrip />
+      {/* <LeadCaptureStrip /> */}
       <ServicesSection />
       <TestimonialsSection />
       <WhyHyderabad />
